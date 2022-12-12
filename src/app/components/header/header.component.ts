eb697fd81f73ab3deee65847a89980512acbb699
aadeshcoder <aadeshtendolkar@gmail.com>
2022-12-12 16:14:16 +0530

    updated with tasks

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  title: string = 'Task Tracker';
+  constructor () {}
 
+  toggleAddTask() {
+    console.log('Toggle Add Class');
+  }
 }
